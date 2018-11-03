da252c75b2e5aea22411059e0f6effc34be895e8
Michael Hefner <michaelphefner@gmail.com>
2018-11-03 19:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function add(one, two){
-  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN'){
+  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN')){
     return add + two;
   }
 }
 function subtract(one, two){
-  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN'){
+  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN')){
     return  two - one;
   }
 }
 function multiply(one, two){
-  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN'){
+  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN')){
     return one * two;
   }
 }
 function divide(one, two){
-  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN'){
+  if((parseInt(one, 10) != 'NaN') && (parseInt(two, 10) != 'NaN')){
     return add / two;
   }
 }
